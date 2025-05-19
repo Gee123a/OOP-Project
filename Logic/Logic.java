719db767343050b07fd8ee7fb8d8cb2cdfb899d4
Gee <ssantoso04@student.ciputra.ac.id>
2025-05-19 23:27:08 +0700

    fondasi file

---
+++ Logic/Logic.java
@@ -0,0 +1,5 @@
+public class Logic {
+    public void run(){
+        System.out.println("Logic is running");
+    }
+}
--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
 
-
-        System.out.println("Ini versi Ivan");
-
+        Logic logic = new Logic();
+        logic.run();
     }
 }
